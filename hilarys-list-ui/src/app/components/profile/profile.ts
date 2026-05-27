831ebb642c3bff0b58e1a01c03a2f79ef34411bc
Aldo Oviedo <aldooviedo98@gmail.com>
2026-05-27 12:38:13 -0700

    better ui in profile page

--- hilarys-list-ui/src/app/components/profile/profile.ts
+++ hilarys-list-ui/src/app/components/profile/profile.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { User } from '../../models/user.model';
 import { AuthService } from '../../services/auth';
+import { CafeService } from '../../services/cafe';
+import { ReviewService } from '../../services/review';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './profile.html',
-  styleUrl: './profile.css'
+  styleUrl: './profile.css',
 })
-export class Profile{
+export class Profile {
+  cafeCount: number = 0;
+  reviewCount: number = 0;
 
-
-
-  constructor(public authService: AuthService) {}
-
-
+  constructor(
+    public authService: AuthService,
+    private cafeService: CafeService,
+    private reviewService: ReviewService,
+    private cdr: ChangeDetectorRef,
+  ) {
+    toObservable(this.authService.currentUser).subscribe((user) => {
+      if (user) {
+        this.cafeService.getCafesByUserId(user.id).subscribe({
+          next: (cafes) => {
+            this.cafeCount = cafes.length;
+            this.cdr.detectChanges();
+          },
+        });
+        this.reviewService.getReviewsByUserId(user.id).subscribe({
+          next: (reviews) => {
+            this.reviewCount = reviews.length;
+            this.cdr.detectChanges();
+          },
+        });
+      }
+    });
+  }
 }
+
+
