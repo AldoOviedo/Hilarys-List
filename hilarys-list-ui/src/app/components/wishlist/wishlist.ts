46c9e74b4ceef3bed56600ad8cbe7604c11bea8b
Aldo Oviedo <aldooviedo98@gmail.com>
2026-05-27 18:01:01 -0700

    Added cafe wishlist feature to website, updated add cafe to reflect this change as well.

---
+++ hilarys-list-ui/src/app/components/wishlist/wishlist.ts
@@ -0,0 +1,37 @@
+import { ChangeDetectorRef, Component, inject, signal } from '@angular/core';
+import { CafeService } from '../../services/cafe';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { AuthService } from '../../services/auth';
+import { Cafe } from '../../models/cafe.model';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-wishlist',
+  imports: [],
+  templateUrl: './wishlist.html',
+  styleUrl: './wishlist.css',
+  standalone: true,
+})
+export class Wishlist {
+  wishlistCafes = signal([]);
+  cafes: Cafe[] = [];
+
+  constructor(
+    private cafeService: CafeService,
+    private authService: AuthService,
+    private cdr: ChangeDetectorRef
+  ) {
+    toObservable(this.authService.currentUser).subscribe((user) => {
+      if (user) {
+        this.cafeService.getWishlist(user.id).subscribe({
+          next: (Cafes) => {
+            this.cafes = Cafes;
+            this.cdr.detectChanges();
+          }
+
+        });
+
+      }
+    });
+  }
+}
